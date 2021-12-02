a97f0ab0e9b5c72ec836dd8d0c402265bbb4538b
leejiwon <irne000805@gmail.com>
2021-12-02 17:53:19 +0900

    BaseActivity

---
+++ app/src/main/java/com/practicejw/withme/BaseActivity.kt
@@ -0,0 +1,27 @@
+package com.practicejw.withme
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.practicejw.withme.api.ServerAPI
+import com.practicejw.withme.api.ServerAPIInterface
+
+class BaseActivity: AppCompatActivity() {
+
+//    서버 기능 목록 -> 물려주자
+
+    lateinit var apiService : ServerAPIInterface
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+//        실제 서버 통신 전담 변수
+        val retrofit = ServerAPI.getRetrofit()
+
+//        기능 목록에 결합해서 -> apiService 에 대입
+        apiService = retrofit.create( ServerAPIInterface::class.java )
+
+
+    }
+
+}

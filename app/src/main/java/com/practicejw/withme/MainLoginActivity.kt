986b11fa55fc87e8d1bb1f7b5f8fbb987657417e
leejiwon <irne000805@gmail.com>
2021-12-02 17:20:17 +0900

    ID/PW 버튼

--- app/src/main/java/com/practicejw/withme/MainLoginActivity.kt
+++ app/src/main/java/com/practicejw/withme/MainLoginActivity.kt
@@ -10,6 +10,16 @@ class MainLoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_login)
 
+        btnLogin.setOnClickListener {
+
+//            입력한 id/pw 변수에 저장
+            val inputId = edtId.text.toString()
+            val InputPw = edtPw.text.toString()
+
+//          서버에 로그인 요청
+
+        }
+
         btnJoin.setOnClickListener {
             val intent = Intent(this, joinFirstActivity::class.java)
             startActivity(intent)

118b819b57c8c8ee886d808082a60a26aae59cb5
leejiwon <irne000805@gmail.com>
2021-12-06 16:45:55 +0900

    my_info UI 대략적

---
+++ app/src/main/java/com/practicejw/withme/MyInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.practicejw.withme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_info_activity)
+    }
+}

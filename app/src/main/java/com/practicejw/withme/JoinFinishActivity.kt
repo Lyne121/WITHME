2de082723f37e2c54d82529206b400994e0299bf
leejiwon <irne000805@gmail.com>
2021-12-06 10:58:28 +0900

    join_finish 버튼 Intent 기능 설정

--- app/src/main/java/com/practicejw/withme/JoinFinishActivity.kt
+++ app/src/main/java/com/practicejw/withme/JoinFinishActivity.kt
@@ -1,11 +1,19 @@
 package com.practicejw.withme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.join_finish_activity.*
 
 class JoinFinishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.join_finish_activity)
+
+        btnToLogin.setOnClickListener {
+            val myIntent = Intent(this@JoinFinishActivity, MainLoginActivity::class.java)
+            startActivity(myIntent)
+
+        }
     }
 }

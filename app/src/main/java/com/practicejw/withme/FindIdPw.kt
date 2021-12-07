eb9edf70c9965d0143a75a1ed09ecab02a188b83
leejiwon <irne000805@gmail.com>
2021-12-07 16:24:48 +0900

    #5 find_Id_Pw 액티비티 생성, 대략적 UI 설정

---
+++ app/src/main/java/com/practicejw/withme/FindIdPw.kt
@@ -0,0 +1,11 @@
+package com.practicejw.withme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindIdPw : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_id_pw)
+    }
+}

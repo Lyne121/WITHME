9349ef2970dba5e4d7965647047a559f74ec87d4
leejiwon <irne000805@gmail.com>
2021-12-07 17:04:11 +0900

    #5 find_Id_Pw : 비밀번호 찾기 기능 추가

--- app/src/main/java/com/practicejw/withme/FindIdPwActivity.kt
+++ app/src/main/java/com/practicejw/withme/FindIdPwActivity.kt
@@ -1,8 +1,14 @@
 package com.practicejw.withme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.practicejw.withme.datas.BasicResponse
 import kotlinx.android.synthetic.main.activity_find_id_pw.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FindIdPwActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +20,38 @@ class FindIdPwActivity : BaseActivity() {
             val inputNick = edtNick_findPw.text.toString()
             val inputEmail = edtEmail_findPw.text.toString()
 
-            apiService.
+            apiService.postRequestFindPw(
+                inputNick,
+                inputEmail
+            ).enqueue(object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
 
+                    if ( response.isSuccessful ) {
+
+                        Toast.makeText(this@FindIdPwActivity , "임시 비밀번호가 이메일로 전송되었습니다",Toast.LENGTH_SHORT).show()
+
+                        val myIntent = Intent(this@FindIdPwActivity , MainLoginActivity::class.java)
+                        startActivity(myIntent)
+
+                        finish()
+
+                    }
+
+                    else{
+                        Toast.makeText(this@FindIdPwActivity, "일치하는 회원 정보가 없습니다. 이름 또는 아이디를 확인해주세요",Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+                override fun onFailure(call: Call<BasicResponse>, t:Throwable) {
+                }
+
+            } )
+
+            }
 
     }
-
 
 }
